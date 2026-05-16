c084b2f44688bedcc75707623161f0bf3e409541
titou579 <titoulefebvre579@outlook.com>
2026-05-16 13:09:23 +0200

    ser

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const webhookURL = "https://discord.com/api/webhooks/1504168506353254421/h8W2Fh2--q0557T2gIm7OYEn5qvQsX-TpY5_oCpmk8q6iHizaO62DpOQ4sFFE6jhrlHT";
+const webhookURL = "https://discord.com/oauth2/authorize?client_id=1503445813253378088&permissions=68624&integration_type=0&scope=bot";
 
 app.post("/message", async (req, res) => {
     const { pseudo, message } = req.body;
